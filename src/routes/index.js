77e6bd2dc01acc7f3cc128c67d91f9160ad6e7d7
Rafael Melón <info@rafaelmelon.es>
2019-11-08 21:30:28 +0200

    server setup deploy

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Home from 'containers/Home';
+
+const Routes = () => (
+
+    <Switch>
+      <Route exact={true} path="/" component={Home} />
+    </Switch>
+
+);
+
+export default Routes;
