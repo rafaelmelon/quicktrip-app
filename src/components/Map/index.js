002c6c47f6d8429732b67bfe7c67244ea88c47e8
Rafael Melón <info@rafaelmelon.es>
2019-11-08 21:14:10 +0200

    get user location

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
-
-const ReactMapbox = ReactMapboxGl({
-  accessToken:
-    'pk.eyJ1IjoicmFmYWVsbWVsb24iLCJhIjoiY2syOWNzNng2MDdqaDNjbWp2czZtcjl0ciJ9.8SuHDi6E9lRERaWVbULzDQ'
-});
+import React, { useState } from 'react';
+import ReactMapGL, { Marker } from "react-map-gl";
 
 const Map = () => {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  });
 
-  return <ReactMapbox
-    style="mapbox://styles/mapbox/streets-v9"
-    containerStyle={{
-      height: '100vh',
-      width: '100vw'
-    }}
+  const [user, setUser] = useState({});
+
+
+  navigator.geolocation.getCurrentPosition(position => {
+    let setUserLocation = {
+        lat: position.coords.latitude,
+        long: position.coords.longitude
+    };
+    let newViewport = {
+      height: "100vh",
+      width: "100vw",
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+      zoom: 10
+    };
+    setViewport(newViewport)
+    setUser(setUserLocation)
+  });
+
+  return <ReactMapGL
+    {...viewport}
+    mapStyle="mapbox://styles/mapbox/outdoors-v11"
+    onViewportChange={viewport => setViewport(viewport)}
+    mapboxApiAccessToken='pk.eyJ1IjoicmFmYWVsbWVsb24iLCJhIjoiY2syOWNzNng2MDdqaDNjbWp2czZtcjl0ciJ9.8SuHDi6E9lRERaWVbULzDQ'
   >
-    <Layer type="symbol" id="marker" layout={{ 'icon-image': 'marker-15' }}>
-      <Feature coordinates={[-0.481747846041145, 51.3233379650232]} />
-    </Layer>
-  </ReactMapbox>;
+    <Marker
+      latitude={viewport.latitude}
+      longitude={viewport.longitude}
+    >
+      I'm Here!!!
+    </Marker>
+  </ReactMapGL>;
 }
 
 export default Map
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 import { App } from 'containers';
 import './index.css';
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 import * as serviceWorker from './serviceWorker';
 
