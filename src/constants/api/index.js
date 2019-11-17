9e092755ad01f7be16f8748d7cadedfad2029fd5
Rafael Melón <info@rafaelmelon.es>
2019-11-17 19:54:17 +0200

    api prod

--- src/constants/api/index.js
+++ src/constants/api/index.js
@@ -2,4 +2,4 @@ export const API_KEY_MAPBOX =
   "pk.eyJ1IjoicmFmYWVsbWVsb24iLCJhIjoiY2syOWNzNng2MDdqaDNjbWp2czZtcjl0ciJ9.8SuHDi6E9lRERaWVbULzDQ";
 
 export const API_QUICKTRIP_DEV = "http://localhost:8080";
-export const API_QUICKTRIP_PROD = "https://rafaelmelon-api.herokuapp.com";
+export const API_QUICKTRIP_PROD = "https://quicktrip-api.herokuapp.com";
